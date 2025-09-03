ddc9d06b42114e0cbf9004c3e7c4f207793168a6
Matwick-15 <mikkelfbe@gmail.com>
2025-09-03 18:34:04 +0200

    inetial

---
+++ main.c
@@ -0,0 +1,39 @@
+// nogen andres kode
+#include <stdio.h>
+
+#include <stdio.h>
+
+int Control(int altitude) {
+  int thruster = 0;
+
+  // min kode
+
+  // tjekker at altitude er både <=100 og >0 og tænder for thruster hvis det er
+  // sandt
+  if (altitude <= 100 && altitude > 0)
+    thruster = 1;
+  // hvis det overstående ikke gælder slukker denne for thruster
+  else
+    thruster = 0;
+
+  // nogen andres kode
+  return thruster;
+}
+
+void Test(int altitude) {
+  int thruster = Control(altitude);
+  int behaviorCorrect = (altitude > 100 && thruster == 0) ||
+                        (altitude <= 100 && altitude > 0 && thruster == 1) ||
+                        (altitude <= 0 && thruster == 0);
+  char *behaviorCorrectIcon = behaviorCorrect ? "ja" : "nej";
+  printf("For altitude %3d, your thruster is %d |%s|\n", altitude, thruster,
+         behaviorCorrectIcon);
+}
+
+int main(void) {
+  Test(150);
+  Test(100);
+  Test(50);
+  Test(0);
+  Test(-1);
+}
